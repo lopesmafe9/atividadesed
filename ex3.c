dc5d2f37416f7c1faca8a6f0151061628ee5459a
mafê <134291043+lopesmafe9@users.noreply.github.com>
2025-01-31 09:49:29 -0300

    Create ex3.c

---
+++ ex3.c
@@ -0,0 +1,31 @@
+--Considere que o perfil de uma pessoa pode ser determinado a partir da sua data de nascimento, conforme exemplificado a seguir. Data uma data de nascimento, informe o perfil correspondente.--
+#include <stdio.h>
+
+int main(){
+
+    int dia, mes, ano, s1, s2, s3;
+    
+    printf("Informe sua data de nascimento: ");
+    printf("\nDia: ");
+    scanf("%d", &dia);
+    printf("Mes: ");
+    scanf("%d", &mes);
+    printf("Ano: ");
+    scanf("%d", &ano);
+
+    printf("\n%d/%d/%d", dia, mes, ano);
+
+    s1 = ((dia * 100) + mes) + ano;
+    s2 = (s1 / 100) + (s1 % 100);
+    s3 = (s2 % 5); 
+
+    switch (s3){
+    case 0: printf("\nTimido"); break;
+    case 1: printf("\nSonhador");break;
+    case 2: printf("\nPaquerador");break;
+    case 3: printf("\nAtraente");break;
+    case 4: printf("\nIrresistivel");break;
+    default:
+        break;
+    }
+}
