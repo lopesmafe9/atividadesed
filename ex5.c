0a3e9947d0312a371be11ee9e460a221481b6529
mafê <134291043+lopesmafe9@users.noreply.github.com>
2025-01-31 10:16:49 -0300

    Update ex5.c

--- ex5.c
+++ ex5.c
@@ -1,4 +1,5 @@
---O quadrado de um número natural n é dado pela soma dos n primeiros números ímpares consecutivos. Por exemplo, 1^2 =1, 2^2 = 1+3 etc. Dado um número n, calcule seu quadrado usando a soma de ímpares ao invés de produto.--
+/*O quadrado de um número natural n é dado pela soma dos n primeiros números ímpares consecutivos. Por exemplo, 1^2 =1, 2^2 = 1+3 etc. Dado um número n, calcule seu quadrado usando a soma de ímpares ao invés de produto.*/
+
 #include <stdio.h>
 
 int main(){
