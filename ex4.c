4734d00aee4f6c1f384c3e1fc65919c56ea84af4
mafê <134291043+lopesmafe9@users.noreply.github.com>
2025-01-31 09:50:34 -0300

    Create ex4.c

---
+++ ex4.c
@@ -0,0 +1,19 @@
+--Dados um número natural n, exiba seu fatorial n!--
+#include <stdio.h>
+
+
+int main(){
+
+    int num, i, fatorial;
+    fatorial = 1;
+
+    printf("Informe o numero: ");
+    scanf("%d", &num);
+
+    for(i = 1; i <= num; i++){
+        fatorial = fatorial *i;
+
+    }
+    printf("O fatorial do numero %d e %d", num, fatorial);
+
+}
