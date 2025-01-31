3205de205999596512a2abae58d107c44debd0df
mafê <134291043+lopesmafe9@users.noreply.github.com>
2025-01-31 10:17:40 -0300

    Update ex6.c

--- ex6.c
+++ ex6.c
@@ -1,4 +1,4 @@
---A série de Fibonacci é 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, … Os dois primeiros termos são iguais a 1, e a partir do terceiro, o termo é dado pela soma dos dois termos anteriores. Dado um número n≥3, exiba o n-ésimo termo da série de Fibonacci. (Não use recursivo.)--
+/*A série de Fibonacci é 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, … Os dois primeiros termos são iguais a 1, e a partir do terceiro, o termo é dado pela soma dos dois termos anteriores. Dado um número n≥3, exiba o n-ésimo termo da série de Fibonacci. (Não use recursivo.)*/
 #include <stdio.h>
 
 int main(){
